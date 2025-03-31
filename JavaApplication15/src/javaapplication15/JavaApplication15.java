a9c10d51add6d05e60bce1719d88751686535fc7
Anthonyy <1602280@senati.pe>
2025-03-31 09:07:06 -0500

    crear la clase conexion y su metodo conectar

--- JavaApplication15/src/javaapplication15/JavaApplication15.java
+++ JavaApplication15/src/javaapplication15/JavaApplication15.java
@@ -15,6 +15,9 @@ public class JavaApplication15 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Conexion cn = new Conexion();
+        
+        cn.conectar();
     }
     
 }
